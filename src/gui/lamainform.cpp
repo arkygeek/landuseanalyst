5f56a5524dbaba98796b980fd20b4185aa3b3db4
timlinuxgis <timlinuxgis@4b9cd65c-31b5-48f6-a99c-4b495aaa06dd>
2007-01-04 17:09:43 +0000

    Initial implementation of a subclass for lamainformbase
    git-svn-id: svn://localhost/tmp/testla/trunk@8 4b9cd65c-31b5-48f6-a99c-4b495aaa06dd

---
+++ src/gui/lamainform.cpp
@@ -0,0 +1,49 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Tim Sutton   *
+ *   tim@linfiniti.com   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "lamainform.h"
+#include <QSettings>
+  LaMainForm::LaMainForm(QWidget* parent, Qt::WFlags fl)
+: QDialog(parent,fl)
+{
+  //required by Qt4 to initialise the ui
+  setupUi(this);
+  readSettings();
+}
+
+LaMainForm::~LaMainForm()
+{
+  writeSettings();
+}
+
+void LaMainForm::readSettings()
+{
+  QSettings mySettings;
+  QPoint pos = mySettings.value("mainwindow/pos", QPoint(200, 200)).toPoint();
+  QSize size = mySettings.value("mainwindow/size", QSize(400, 400)).toSize();
+  resize(size);
+  move(pos);
+}
+
+void LaMainForm::writeSettings()
+{
+  QSettings mySettings;
+  mySettings.setValue("mainwindow/pos", pos());
+  mySettings.setValue("mainwindow/size", size());
+}
