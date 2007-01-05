514585331074bac68b68a594c2d1a4f3d3c13b41
arkygeek <arkygeek@4b9cd65c-31b5-48f6-a99c-4b495aaa06dd>
2007-01-05 10:04:37 +0000

    calculates pig model when view pressed.  still needs to incorporate kill weight
    git-svn-id: svn://localhost/tmp/testla/trunk@20 4b9cd65c-31b5-48f6-a99c-4b495aaa06dd

--- src/gui/lamainform.cpp
+++ src/gui/lamainform.cpp
@@ -56,25 +56,49 @@ void LaMainForm::on_calculate_button_clicked()
 void LaMainForm::on_pigview_clicked()
 {
 	qDebug("Jason Rocks");
-/*
-	int myPigSnoutLength = spinSnoutLength->value();
-pigpercent
-piglittersize
-pigweight
-pigfodderuse
-pigfodderamount
-pigfoddercrop
-pigfoddertime
-piggrazefallow
-pigraster
-dietslider   for overall MEAT value
-meatslider   for tame percentage
-dailycalories
-*/
+	int mdp = dietslider->value(); //grab value from slider for overall meat percentage
+	int mtp = meatslider->value(); //grab value from slider for tame meat percentage
+	int pp = pigpercent->value(); //grab value from form for percentage of pigmeat of meat portion of diet
+	int pls = piglittersize->value(); //grab value from form for pig litter size
+	int pw = pigweight->value(); //grab value from form for for pig kill weight
+	int pgt = piggrowtime->value(); //grab value from form for for pig grow time
+	int cal = dailycalories->value();
+	int popn = population->value();
+	//int pfflag = pigfodderuse->value(); //grab value from form for fodder use flag
+	int pfa = pigfodderamount->value(); //grab value from form for amount of fodder
+	//int pfc = pigfoddercrop->value(); //grab value from form for type of fodder
+	//int pft = pigfoddertime->value(); //grab value from form for time measurement of fodder rate
+	//int pgrflag = piggrazefallow->value(); //grab value from form for fallow grazing flag
+	float meat = (((((mdp/100.)*(mtp/100.)*(pp/100.))*cal*popn)/3000.)*365.*2.); // 3000 is calories/kg of pork
+	float animals = (meat/100.);
+	float sows = ((10.*meat)/(1760.53*pls));
+	float drysows = ((sows*30.80)/249.70);
+	float gsows = ((sows*41.6)/249.70);
+	float lsows = ((sows*177.20)/249.70);
+	float spigs = (((355.10/41.60)/10.)*pls*lsows);
+	float npigs = ((((sows*420.10)/249.70)/10.)*pls);
+	float gpigs = ((((sows*1414.70)/249.70)/10.)*pls);
+	float total = (sows+spigs+npigs+gpigs);
 
-//float myPigSnoutLength = spinSnoutLength->value();
-
-
+	qDebug("You supplied me with this information:");
+	qDebug("Population of Settlement: " + QString::number(popn).toLocal8Bit());
+	qDebug("Calories/person per day: " + QString::number(cal).toLocal8Bit());
+	qDebug("Percentage of Calories in the diet from MEAT: " + QString::number(mdp).toLocal8Bit());
+	qDebug("Percentage of MEAT that is from domesticated animals: " + QString::number(mtp).toLocal8Bit());
+	qDebug("Percentage of DOMESTICATED MEAT that is from PIGS: " + QString::number(pp).toLocal8Bit());
+	qDebug("Calories per kg in PIG MEAT: 3000 assumed");
+	qDebug("Average Litter Size: " + QString::number(pls).toLocal8Bit());
+	qDebug("kg of meat per year: " + QString::number(meat).toLocal8Bit());
+	qDebug("Number of 100kg pigs: " + QString::number(animals).toLocal8Bit());
+	qDebug("Sows required to produce this much meat: " + QString::number(sows).toLocal8Bit());
+	qDebug("Non-Pregnant sows and gilts: " + QString::number(drysows).toLocal8Bit());
+	qDebug("Gestating Sows: " + QString::number(gsows).toLocal8Bit());
+	qDebug("Lactating Sows: " + QString::number(lsows).toLocal8Bit());
+	qDebug("Total Adult Females: " + QString::number(sows).toLocal8Bit());
+	qDebug("Suckling Pigs: " + QString::number(spigs).toLocal8Bit());
+	qDebug("Nursery Pigs: " + QString::number(npigs).toLocal8Bit());
+	qDebug("Growing and finishing pigs: " + QString::number(gpigs).toLocal8Bit());
+	qDebug("Total pigs: " + QString::number(total).toLocal8Bit());
 }
 
 void LaMainForm::on_meatslider_valueChanged(int theValue)
