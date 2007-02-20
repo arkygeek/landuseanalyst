929280143177d78b5b12de7347b3b17b50bd1817
arkygeek <arkygeek@4b9cd65c-31b5-48f6-a99c-4b495aaa06dd>
2007-02-20 17:44:16 +0000

    checking to see if this is ok
    git-svn-id: svn://localhost/tmp/testla/trunk@149 4b9cd65c-31b5-48f6-a99c-4b495aaa06dd

--- src/gui/laanimal.h
+++ src/gui/laanimal.h
@@ -43,18 +43,32 @@ class LaAnimal : public LaSerialisable
 
     /** The name of this animal */
     QString name() const;
-    int usableMeat() const;
+
+    /** Percentage of animals live weight that can be utilized for meat */
+    int mUsableMeat() const;
+    /** The weight in kg at which the animals are slaughtered */
     int killWeight() const;
+    /** The number of weeks from birth it takes to attain slaughtering weight */
     int growTime() const;
+    /** The percentage of babies that die before being usable as either meat or breeding */
     int deathRate() const;
+    /** The number of calories a gestating female requires per day */
     int gestating() const;
+    /** The number of calories a lactating female requires per day */
     int lactating() const;
+    /** The number of calories a juvenile requires per day */
     int juvenile() const;
+    /** The life expectancy in years of the animal */
     int lifeExpectancy() const;
+    /** The number of years a female will reliably produce offspring */
     int breedingExpectancy() const;
+    /** The average number of young produced per pregnancy */
     int youngPerBirth() const;
+    /** The age in weeks at which babies stop suckling */
     int weaningAge() const;
+    /** The number of days required for gestation */
     int gestationTime() const;
+    /** The number of days in the female estrous cycle */
     int estrousCycle() const;
 
     //
@@ -65,21 +79,58 @@ class LaAnimal : public LaSerialisable
      * @see name()
      */
     void setName(QString theName);
+
     /** Set the useable meat as a percentage
      * @see UsableMeat()
      */
     void setUsableMeat(int thePercentage);
+    /** Set the useable meat as a percentage
+     * @see UsableMeat()
+     */
     void setKillWeight(QString theName);
+    /** Set the useable meat as a percentage
+     * @see UsableMeat()
+     */
     void setGrowTime(QString theName);
+    /** Set the useable meat as a percentage
+     * @see UsableMeat()
+     */
     void setDeathRate(QString theName);
+    /** Set the useable meat as a percentage
+     * @see UsableMeat()
+     */
     void setGestating(QString theName);
+    /** Set the useable meat as a percentage
+     * @see UsableMeat()
+     */
     void setLactating(QString theName);
+    /** Set the useable meat as a percentage
+     * @see UsableMeat()
+     */
     void setJuvenile(QString theName);
+    /** Set the useable meat as a percentage
+     * @see UsableMeat()
+     */
     void setLifeExpectancy(QString theName);
+    /** Set the useable meat as a percentage
+     * @see UsableMeat()
+     */
     void setBreedingExpectancy(QString theName);
+    /** Set the useable meat as a percentage
+     * @see UsableMeat()
+     */
     void setYoungPerBirth(QString theName);
+    /** Set the useable meat as a percentage
+     * @see UsableMeat()
+     */
     void setWeaningAge(QString theName);
+    /** Set the useable meat as a percentage
+     * @see UsableMeat()
+     */
     void setGestationTime(QString theName);
+    /** Set the useable meat as a percentage
+     * @see UsableMeat()
+     */
     void setEstrousCycle(QString theName);
 
     /** Return an xml representation of this layer
@@ -94,8 +145,34 @@ class LaAnimal : public LaSerialisable
      */
     bool fromXml(const QString theXml){ return false; };
   private:
-    /** The name for this layer - usually a full path and filename */
+    /** The name for this animal */
     QString mName;
+    /** Percentage of animals live weight that can be utilized for meat */
+    int mUsableMeat;
+    /** The weight in kg at which the animals are slaughtered */
+    int killWeight;
+    /** The number of weeks from birth it takes to attain slaughtering weight */
+    int growTime;
+    /** The percentage of babies that die before being usable as either meat or breeding */
+    int deathRate;
+    /** The number of calories a gestating female requires per day */
+    int gestating;
+    /** The number of calories a lactating female requires per day */
+    int lactating;
+    /** The number of calories a juvenile requires per day */
+    int juvenile;
+    /** The life expectancy in years of the animal */
+    int lifeExpectancy;
+    /** The number of years a female will reliably produce offspring */
+    int breedingExpectancy;
+    /** The average number of young produced per pregnancy */
+    int youngPerBirth;
+    /** The age in weeks at which babies stop suckling */
+    int weaningAge;
+    /** The number of days required for gestation */
+    int gestationTime;
+    /** The number of days in the female estrous cycle */
+    int estrousCycle;
 };
 
 #endif //LAANIMAL_H
