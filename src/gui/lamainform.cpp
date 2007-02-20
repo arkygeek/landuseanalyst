4c5b9d106cc6aa89612b1d9289a1b478022bab9b
arkygeek <arkygeek@4b9cd65c-31b5-48f6-a99c-4b495aaa06dd>
2007-02-20 13:35:20 +0000

    not all working
    git-svn-id: svn://localhost/tmp/testla/trunk@139 4b9cd65c-31b5-48f6-a99c-4b495aaa06dd

--- src/gui/lamainform.cpp
+++ src/gui/lamainform.cpp
@@ -17,11 +17,13 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
+
 //app includes
 #include "lamainform.h"
 #include "version.h"
 #include "laanimalmain.h"
 #include "laplantmain.h"
+
 //qt includes
 #include <QSettings>
 #include <QTreeWidget>
