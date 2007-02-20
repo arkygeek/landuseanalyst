b64fea3096c83560d6ed2114f726c96f8f62a4d8
arkygeek <arkygeek@4b9cd65c-31b5-48f6-a99c-4b495aaa06dd>
2007-02-20 23:59:52 +0000

    how could i forget these files? :P
    git-svn-id: svn://localhost/tmp/testla/trunk@154 4b9cd65c-31b5-48f6-a99c-4b495aaa06dd

---
+++ src/gui/laplant.cpp
@@ -0,0 +1,191 @@
+/***************************************************************************
+                          omgserialisable.cpp  -  description
+                             -------------------
+    begin                : March 2006
+    copyright            : (C) 2003 by Tim Sutton
+    email                : tim@linfiniti.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include <QString>
+#include "laplant.h"
+
+LaPlant::LaPlant() : LaSerialisable()
+{
+
+}
+LaPlant::~LaPlant()
+{
+
+}
+
+//copy constructor
+LaPlant::LaPlant(const LaPlant& thePlant)
+{
+  mName=thePlant.name();
+  mCropYield=thePlant.cropYield();
+  mCropCalories=thePlant.cropCalories();
+  mCropFodderProduction=thePlant.fodderProduction();
+  mCropFodderCalories=thePlant.fodderCalories();
+  mYie;=thePlant.yieldUnits();
+}
+
+LaPlant& LaPlant::operator=(const LaPlant& thePlant)
+{
+  if (this == &thePlant) return *this;   // Gracefully handle self assignment
+
+  mName=thePlant.name();
+  m=thePlant.(cropYield);
+  m=thePlant.(cropCalories);
+  m=thePlant.(fodderProduction);
+  m=thePlant.(fodderCalories);
+  m=thePlant.(yieldUnits);
+  return *this;
+}
+
+QString LaPlant::name() const
+{
+  return mName;
+}
+int LaPlant::usableMeat() const
+{
+  return mUsableMeat;
+}
+int LaPlant::killWeight() const
+{
+  return mKillWeight;
+}
+int LaPlant::growTime() const
+{
+  return mGrowTime;
+}
+int LaPlant::deathRate() const
+{
+  return mDeathRate;
+}
+int LaPlant::gestating() const
+{
+  return mGestating;
+}
+int LaPlant::lactating() const
+{
+  return mLactating;
+}
+int LaPlant::juvenile() const
+{
+  return mJuvenile;
+}
+int LaPlant::lifeExpectancy() const
+{
+  return mLifeExpectancy;
+}
+int LaPlant::breedingExpectancy() const
+{
+  return mBreedingExpectancy;
+}
+int LaPlant::youngPerBirth() const
+{
+  return mYoungPerBirth;
+}
+int LaPlant::weaningAge() const
+{
+  return mWeaningAge;
+}
+int LaPlant::gestationTime() const
+{
+  return mGestationTime;
+}
+int LaPlant::estrousCycle() const
+{
+  return mEstrousCycle;
+}
+void LaPlant::setName(QString theName)
+{
+  mName=theName;
+}
+void LaPlant::setUsableMeat(int thePercentage)
+{
+  if (thePercentage > 100) {thePercentage=100;}
+  if (thePercentage < 0) {thePercentage=100;}
+  mUsableMeat=thePercentage;
+}
+
+void LaPlant::setKillWeight(int theKg)
+{
+  mKillWeight=theKg;
+}
+void LaPlant::setGrowTime(int theWeeks)
+{
+  mGrowTime=theWeeks;
+}
+void LaPlant::setDeathRate(int thePercentage)
+{
+  mDeathRate=thePercentage;
+}
+void LaPlant::setGestating(int theCalories)
+{
+  mGestating=theCalories;
+}
+void LaPlant::setLactating(int theCalories)
+{
+  mLactating=theCalories;
+}
+void LaPlant::setJuvenile(int theCalories)
+{
+  mJuvenile=theCalories;
+}
+void LaPlant::setLifeExpectancy(int theYears)
+{
+  mLifeExpectancy=theYears;
+}
+void LaPlant::setBreedingExpectancy(int theYears)
+{
+  mBreedingExpectancy=theYears;
+}
+void LaPlant::setYoungPerBirth(int theInteger)
+{
+  mYoungPerBirth=theInteger;
+}
+void LaPlant::setWeaningAge(int theWeeks)
+{
+  mWeaningAge=theWeeks;
+}
+void LaPlant::setGestationTime(int theDays)
+{
+  mGestationTime=theDays;
+}
+void LaPlant::setEstrousCycle(int theDays)
+{
+  mEstrousCycle=theDays;
+}
+
+
+
+QString LaPlant::toXml()
+{
+  QString myString = QString("<Plant Id=\"" + mName + "\"/>\n");
+          myString+=QString("<plant>\n");
+            myString+=QString("  <name>" + mName + "</name>\n");
+            myString+=QString("  <usableMeat>" + QString::number(mUsableMeat) + "</usableMeat>\n");
+            myString+=QString("  <killWeight>" + QString::number(mKillWeight) + "</killWeight>\n");
+            myString+=QString("  <growTime>" + QString::number(mGrowTime) + "</growTime>\n");
+            myString+=QString("  <deathRate>" + QString::number(mDeathRate) + "</deathRate>\n");
+            myString+=QString("  <gestating>" + QString::number(mGestating) + "</gestating>\n");
+            myString+=QString("  <lactating>" + QString::number(mLactating) + "</lactating>\n");
+            myString+=QString("  <juvenile>" + QString::number(mJuvenile) + "</juvenile>\n");
+            myString+=QString("  <lifeExpectancy>" + QString::number(mLifeExpectancy) + "</lifeExpectancy>\n");
+            myString+=QString("  <breedingLife>" + QString::number(mBreedingExpectancy) + "</breedingLife>\n");
+            myString+=QString("  <youngPerBirth>" + QString::number(mYoungPerBirth) + "</youngPerBirth>\n");
+            myString+=QString("  <weaningAge>" + QString::number(mWeaningAge) + "</weaningAge>\n");
+            myString+=QString("  <gestationTime>" + QString::number(mGestationTime) + "</gestationTime>\n");
+            myString+=QString("  <estrousCycle>" + QString::number(mEstrousCycle) + "</estrousCycle>\n");
+          myString+=QString("</plant>\n");
+  return myString;
+}
---
+++ src/gui/laplant.h
@@ -0,0 +1,192 @@
+/***************************************************************************
+                          laplant.h  -  An plant class
+                             -------------------
+    begin                : March 2006
+    copyright            : (C) 2003 by Tim Sutton
+    email                : tim@linfiniti.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef LAPLANT_H
+#define LAPLANT_H
+
+class QString;
+#include "laserialisable.h"
+#include <QString>
+/** 
+  * An class to represent an plant
+  * @author Tim Sutton
+  */
+
+class LaPlant : public LaSerialisable 
+{
+  public:
+    /** Constructor . */
+    LaPlant();
+    /** Desctructor . */
+    ~LaPlant();
+    /** copy constructor */
+    LaPlant(const LaPlant& thePlant); 
+    /** Assignement operator */
+    LaPlant& operator= (const LaPlant& thePlant);
+    
+    //
+    // Accessors
+    //
+
+    /** The name of this plant */
+    QString name() const;
+    /** Percentage of plants live weight that can be utilized for meat */
+    int usableMeat() const;
+    /** The weight in kg at which the plants are slaughtered */
+    int killWeight() const;
+    /** The number of weeks from birth it takes to attain slaughtering weight */
+    int growTime() const;
+    /** The percentage of babies that die before being usable as either meat or breeding */
+    int deathRate() const;
+    /** The number of calories a gestating female requires per day */
+    int gestating() const;
+    /** The number of calories a lactating female requires per day */
+    int lactating() const;
+    /** The number of calories a juvenile requires per day */
+    int juvenile() const;
+    /** The life expectancy in years of the plant */
+    int lifeExpectancy() const;
+    /** The number of years a female will reliably produce offspring */
+    int breedingExpectancy() const;
+    /** The average number of young produced per pregnancy */
+    int youngPerBirth() const;
+    /** The age in weeks at which babies stop suckling */
+    int weaningAge() const;
+    /** The number of days required for gestation */
+    int gestationTime() const;
+    /** The number of days in the female estrous cycle */
+    int estrousCycle() const;
+
+    //
+    // Mutators
+    //
+
+    /** Set the plantName
+     * @see name()
+     */
+    void setName(QString theName);
+
+    /** Set useableMeat as a percentage
+     * @see usableMeat()
+     */
+    void setUsableMeat(int thePercentage);
+
+    /** Set killWeight as kg 
+     * @see killWeight()
+     */
+    void setKillWeight(int theKg);
+
+    /** Set the growTime in weeks
+     * @see growTime()
+     */
+    void setGrowTime(int theWeeks);
+
+    /** Set the deathRate as a percentage
+     * @see deathRate()
+     */
+    void setDeathRate(int thePercentage);
+
+    /** Set the daily calories required for a gestating female
+     * @see gestating()
+     */
+    void setGestating(int theCalories);
+
+    /** Set the daily calories required for a lactating female
+     * @see UsableMeat()
+     */
+    void setLactating(int theCalories);
+
+    /** Set the daily calories required for a juvenile
+     * @see UsableMeat()
+     */
+    void setJuvenile(int theCalories);
+
+    /** Set the average lifeExpectancy in years
+     * @see lifeExpectancy()
+     */
+    void setLifeExpectancy(int theYears);
+
+    /** Set the average breedingLifeExpectancy in years
+     * @see breedingExpectancy()
+     */
+    void setBreedingExpectancy(int theYears);
+
+    /** Set the average number of youngPerBirth
+     * @see youngPerBirth()
+     */
+    void setYoungPerBirth(int theInteger);
+
+    /** Set the weaningAge in weeks
+     * @see weaningAge()
+     */
+    void setWeaningAge(int theWeeks);
+
+    /** Set the gestationTime in Days
+     * @see gestationTime()
+     */
+    void setGestationTime(int theDays);
+
+    /** Set the estrousCycle in days
+     * @see estrousCycle()
+     */
+    void setEstrousCycle(int theDays);
+
+    /** Return an xml representation of this layer
+     * @NOTE this class inherits the serialisable interface so
+     * it MUST implement this
+     */
+    QString toXml();
+
+    /** Read this object from xml and return result as true for success, false for failure.
+     * @see LaSerialisable
+     * @NOTE this class inherits the serialisable interface so
+     * it MUST implement this
+     */
+    bool fromXml(const QString theXml){ return false; };
+  private:
+    /** The name for this plant */
+    QString mName;
+    /** Percentage of plants live weight that can be utilized for meat */
+    int mUsableMeat;
+    /** The weight in kg at which the plants are slaughtered */
+    int mKillWeight;
+    /** The number of weeks from birth it takes to attain slaughtering weight */
+    int mGrowTime;
+    /** The percentage of babies that die before being usable as either meat or breeding */
+    int mDeathRate;
+    /** The number of calories a gestating female requires per day */
+    int mGestating;
+    /** The number of calories a lactating female requires per day */
+    int mLactating;
+    /** The number of calories a juvenile requires per day */
+    int mJuvenile;
+    /** The life expectancy in years of the plant */
+    int mLifeExpectancy;
+    /** The number of years a female will reliably produce offspring */
+    int mBreedingExpectancy;
+    /** The average number of young produced per pregnancy */
+    int mYoungPerBirth;
+    /** The age in weeks at which babies stop suckling */
+    int mWeaningAge;
+    /** The number of days required for gestation */
+    int mGestationTime;
+    /** The number of days in the female estrous cycle */
+    int mEstrousCycle;
+};
+
+#endif //LAPLANT_H
+
