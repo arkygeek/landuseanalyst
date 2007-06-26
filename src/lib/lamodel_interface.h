22509a440d7b40e15d5e75e82aebeedffb44733c
jjorgenson <jjorgenson@4b9cd65c-31b5-48f6-a99c-4b495aaa06dd>
2007-06-26 08:23:10 +0000

    added reports class
    git-svn-id: svn://localhost/tmp/testla/trunk@393 4b9cd65c-31b5-48f6-a99c-4b495aaa06dd

---
+++ src/lib/lamodel_interface.h
@@ -0,0 +1,17 @@
+#ifndef __LAMODEL_INTERFACE_H
+#define __LAMODEL_INTERFACE_H
+
+class LaModelInterface
+{
+public:
+    LaModelInterface() {}
+    virtual ~LaModelInterface() {}
+
+
+private:
+    LaModelInterface( const LaModelInterface& source );
+    void operator = ( const LaModelInterface& source );
+};
+
+
+#endif // __LAMODEL_INTERFACE_H
