ac7bd523cc5fd583607400247062be46bbb4c798
arkygeek <arkygeek@4b9cd65c-31b5-48f6-a99c-4b495aaa06dd>
2009-01-25 23:27:08 +0000

    I guess it would be useful to actually include the class in the commit :-P
    git-svn-id: svn://localhost/tmp/testla/trunk@617 4b9cd65c-31b5-48f6-a99c-4b495aaa06dd

---
+++ src/lib/ladietlabels.cpp
@@ -0,0 +1,137 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by: Jason Jorgenson   arkygeek@gmail.com           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "ladietlabels.h"
+
+LaDietLabels::LaDietLabels()
+{
+}
+
+LaDietLabels::~LaDietLabels()
+{
+}
+
+  //accessors
+
+float LaDietLabels::dairyMCalories() const
+{
+  return mDairyMCalories;
+}
+float LaDietLabels::cropMCalories() const
+{
+  return mCropMCalories;
+}
+float LaDietLabels::wildAnimalMCalories() const
+{
+  return mWildAnimalMCalories;
+}
+float LaDietLabels::wildPlantsMCalories() const
+{
+  return mWildPlantsMCalories;
+}
+float LaDietLabels::dairyPortionPct() const
+{
+  return mDairyPortionPct;
+}
+float LaDietLabels::tameMeatPortionPct() const
+{
+  return mTameMeatPortionPct;
+}
+float LaDietLabels::cropsPortionPct() const
+{
+  return mCropsPortionPct;
+}
+float LaDietLabels::wildAnimalPortionPct() const
+{
+  return mWildAnimalPortionPct;
+}
+float LaDietLabels::wildPlantsPortionPct() const
+{
+  return mWildPlantsPortionPct;
+}
+float LaDietLabels::plantsPortionPct() const
+{
+  return mPlantsPortionPct;
+}
+float LaDietLabels::animalPortionPct() const
+{
+  return mAnimalPortionPct;
+}
+float LaDietLabels::kiloCaloriesIndividualAnnual() const
+{
+  return mKiloCaloriesIndividualAnnual;
+}
+float LaDietLabels::megaCaloriesSettlementAnnual() const
+{
+  return mMegaCaloriesSettlementAnnual;
+}
+
+  //mutators
+
+void LaDietLabels::setDairyMCalories(float theDairyMCalories)
+{
+  mDairyMCalories = theDairyMCalories;
+}
+void LaDietLabels::setCropMCalories(float theCropMCalories)
+{
+  mCropMCalories = theCropMCalories;
+}
+void LaDietLabels::setWildAnimalMCalories(float theWildAnimalMCalorie)
+{
+  mWildAnimalMCalories = theWildAnimalMCalorie;
+}
+void LaDietLabels::setWildPlantsMCalories(float theWildPlantsMCalories)
+{
+  mWildPlantsMCalories = theWildPlantsMCalories;
+}
+void LaDietLabels::setDairyPortionPct(float theDairyPortionPct)
+{
+  mDairyPortionPct = theDairyPortionPct;
+}
+void LaDietLabels::setTameMeatPortionPct(float theTameMeatPortionPct)
+{
+  mTameMeatPortionPct = theTameMeatPortionPct;
+}
+void LaDietLabels::setCropsPortionPct(float theCropsPortionPct)
+{
+  mCropsPortionPct = theCropsPortionPct;
+}
+void LaDietLabels::setWildAnimalPortionPct(float theWildAnimalPortionPct)
+{
+  mWildAnimalPortionPct = theWildAnimalPortionPct;
+}
+void LaDietLabels::setWildPlantsPortionPct(float theWildPlantsPortionPct)
+{
+  mWildPlantsPortionPct = theWildPlantsPortionPct;
+}
+void LaDietLabels::setPlantsPortionPct(float thePlantsPortionPct)
+{
+  mPlantsPortionPct = thePlantsPortionPct;
+}
+void LaDietLabels::setAnimalPortionPct(float theAnimalPortionPct)
+{
+  mAnimalPortionPct = theAnimalPortionPct;
+}
+void LaDietLabels::setKiloCaloriesIndividualAnnual(float theKiloCaloriesIndividualAnnual)
+{
+  mKiloCaloriesIndividualAnnual = theKiloCaloriesIndividualAnnual;
+}
+void LaDietLabels::setMegaCaloriesSettlementAnnual(float theMegaCaloriesSettlementAnnua)
+{
+  mMegaCaloriesSettlementAnnual = theMegaCaloriesSettlementAnnua;
+}
---
+++ src/lib/ladietlabels.h
@@ -0,0 +1,79 @@
+/***************************************************************************
+ *   Copyright  (C) 2007 by: Jason Jorgenson   arkygeek@gmail.com           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *    (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef LADIETLABELS_H
+#define LADIETLABELS_H
+#include <QString>
+/**
+ * This class represents all of the required data for setting labels in the diet tab.
+ *
+ * @author Jason Jorgenson
+ */
+class LaDietLabels{
+public:
+  LaDietLabels();
+
+  ~LaDietLabels();
+
+  // accessors
+  float dairyMCalories() const;
+  float cropMCalories() const;
+  float wildAnimalMCalories() const;
+  float wildPlantsMCalories() const;
+  float dairyPortionPct() const;
+  float tameMeatPortionPct() const;
+  float cropsPortionPct() const;
+  float wildAnimalPortionPct() const;
+  float wildPlantsPortionPct() const;
+  float plantsPortionPct() const;
+  float animalPortionPct() const;
+  float kiloCaloriesIndividualAnnual() const;
+  float megaCaloriesSettlementAnnual() const;
+
+  // mutators
+  void setDairyMCalories (float theDairyMCalories);
+  void setCropMCalories (float theCropMCalories);
+  void setWildAnimalMCalories (float theWildAnimalMCalorie);
+  void setWildPlantsMCalories (float theWildPlantsMCalories);
+  void setDairyPortionPct (float theDairyPortionPct);
+  void setTameMeatPortionPct (float theTameMeatPortionPct);
+  void setCropsPortionPct (float theCropsPortionPct);
+  void setWildAnimalPortionPct (float theWildAnimalPortionPct);
+  void setWildPlantsPortionPct (float theWildPlantsPortionPct);
+  void setPlantsPortionPct (float thePlantsPortionPct);
+  void setAnimalPortionPct (float theAnimalPortionPct);
+  void setKiloCaloriesIndividualAnnual (float theKCaloriesIndividualAnnual);
+  void setMegaCaloriesSettlementAnnual (float theMCaloriesSettlementAnnua);
+
+  private:
+  float mDairyMCalories;
+  float mCropMCalories;
+  float mWildAnimalMCalories;
+  float mWildPlantsMCalories;
+  float mDairyPortionPct;
+  float mTameMeatPortionPct;
+  float mCropsPortionPct;
+  float mWildAnimalPortionPct;
+  float mWildPlantsPortionPct;
+  float mPlantsPortionPct;
+  float mAnimalPortionPct;
+  float mKiloCaloriesIndividualAnnual;
+  float mMegaCaloriesSettlementAnnual;
+};
+
+#endif
