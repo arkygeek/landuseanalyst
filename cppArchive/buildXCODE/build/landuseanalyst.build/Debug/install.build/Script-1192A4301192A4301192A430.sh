59ccc169cfbbdfda0e621fbf20ce3d83d7e42c3e
Jason Jorgenson <jjorgenson@gmail.com>
2025-03-18 11:09:38 -0600

    trimmed a bunch of unneeded files from this branch.

--- cppArchive/buildXCODE/build/landuseanalyst.build/Debug/install.build/Script-1192A4301192A4301192A430.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/arkygeek/devel/landuseanalyst/buildXCODE -f /Users/arkygeek/devel/landuseanalyst/buildXCODE/CMakeScripts/install_postBuildPhase.make$CONFIGURATION all
--- cppArchive/buildXCODE/src/gui/landuseanalyst.build/Debug/landuseanalyst.build/Script-FFD4930FFD4930FFD4930000.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/arkygeek/devel/landuseanalyst/buildXCODE/src/gui -f /Users/arkygeek/devel/landuseanalyst/buildXCODE/src/gui/CMakeScripts/landuseanalyst_postBuildPhase.make$CONFIGURATION all
