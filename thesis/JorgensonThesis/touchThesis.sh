7895c8bf32f69d93d844f13df3d0f50f6e011eff
Jason Jorgenson <jjorgenson@gmail.com>
2026-05-14 09:22:51 -0600

    feat: add thesis analysis maps, charts, and methodology images

---
+++ thesis/JorgensonThesis/touchThesis.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd ~/Documents/JorgensonThesis
+touch 1Introduction/Introduction.tex
+touch 2SouthernLevant/Overview.tex 
+touch 3GIS/GIS.tex
+touch 4MethodologyOverview/MethodologyOverview.tex 
+touch 5EBAShuna/Shuna.tex
+touch 6Networks/Networks.tex
+touch 7Conclusions/Conclusions.tex 
+touch 9BackMatter/appendix.tex 
